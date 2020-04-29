a9e235fbc97b2625ba227c5a673f5937b2a53b7e
Mateusz Tomczyk <mateusz.tomczyk@poczta.fm>
2020-04-29 23:13:40 +0200

    [impr] recomputing tile movement costs when tile is updated

--- src/app/game/tiles-manager.ts
+++ src/app/game/tiles-manager.ts
@@ -42,6 +42,11 @@ export class TilesManager {
   }
 
   updateTile(tile: Tile) {
+    tile.computeMovementCosts();
+    for (const neighbour of tile.neighbours) {
+      // TODO this loop can be optimized by computing only the cost from neighbour to tile.
+      neighbour.computeMovementCosts();
+    }
     this._updatedTile$.next(tile);
   }
 
