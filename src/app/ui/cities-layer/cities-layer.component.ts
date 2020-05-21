81b3822d9231e50f4a8a67dab960ac9ba86d628e
Mateusz Tomczyk <mateusz.tomczyk@poczta.fm>
2020-05-21 23:21:58 +0200

    [fix] fixed city labels not showing up until camera movement

--- src/app/ui/cities-layer/cities-layer.component.ts
+++ src/app/ui/cities-layer/cities-layer.component.ts
@@ -57,42 +57,46 @@ export class CitiesLayerComponent implements OnInit, AfterViewInit {
   }
 
   ngAfterViewInit() {
-    this.camera.transform$.pipe(takeUntil(this.ngUnsubscribe)).subscribe(() => {
-      const el = this.elementRef.nativeElement;
-      let scale = this.camera.transform.scale;
-      const box = this.camera.tileBoundingBox;
+    this.camera.transform$
+      .pipe(takeUntil(this.ngUnsubscribe))
+      .subscribe(() => this.updateTransforms());
+  }
 
-      let opacity = 1;
-      if (scale < 30) {
-        opacity = Math.max(0, 1 - (30 - scale) / 8);
+  updateTransforms() {
+    const el = this.elementRef.nativeElement;
+    let scale = this.camera.transform.scale;
+    const box = this.camera.tileBoundingBox;
+
+    let opacity = 1;
+    if (scale < 30) {
+      opacity = Math.max(0, 1 - (30 - scale) / 8);
+    }
+    el.style.opacity = opacity.toString();
+
+    if (opacity === 0) {
+      return;
+    }
+
+    for (const cityComponent of this.citiesComponents) {
+      const tile = cityComponent.city.tile;
+      const cityEl = cityComponent.elementRef.nativeElement;
+      if (
+        tile.x >= box.xStart &&
+        tile.x <= box.xEnd &&
+        tile.y >= box.yStart &&
+        tile.y <= box.yEnd
+      ) {
+        cityEl.style.display = "flex";
+      } else {
+        cityEl.style.display = "none";
+        continue;
       }
-      el.style.opacity = opacity.toString();
 
-      if (opacity === 0) {
-        return;
-      }
-
-      for (const cityComponent of this.citiesComponents) {
-        const tile = cityComponent.city.tile;
-        const cityEl = cityComponent.elementRef.nativeElement;
-        if (
-          tile.x >= box.xStart &&
-          tile.x <= box.xEnd &&
-          tile.y >= box.yStart &&
-          tile.y <= box.yEnd
-        ) {
-          cityEl.style.display = "flex";
-        } else {
-          cityEl.style.display = "none";
-          continue;
-        }
-
-        const cityScale = Math.pow(scale / 70, 0.4);
-        let [x, y] = getTileCoords(cityComponent.city.tile);
-        [x, y] = this.camera.canvasToScreen(x + 0.5, y + 0.8);
-        cityEl.style.transform = `translate(${x}px, ${y}px) scale(${cityScale})`;
-      }
-    });
+      const cityScale = Math.pow(scale / 70, 0.4);
+      let [x, y] = getTileCoords(cityComponent.city.tile);
+      [x, y] = this.camera.canvasToScreen(x + 0.5, y + 0.8);
+      cityEl.style.transform = `translate(${x}px, ${y}px) scale(${cityScale})`;
+    }
   }
 
   ngOnDestroy() {
@@ -106,6 +110,8 @@ export class CitiesLayerComponent implements OnInit, AfterViewInit {
       player.exploredTiles.has(city.tile),
     );
     this.cdr.markForCheck();
+
+    setTimeout(() => this.updateTransforms());
   }
 
   trackByCityId(index: number, city: City) {
